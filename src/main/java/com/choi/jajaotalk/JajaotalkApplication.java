9d7537d8cfe6d612db38830029772d33fdde33f7
choi0926 <ghtjd753753@gmail.com>
2020-07-03 00:43:51 +0900

    초기 세팅

---
+++ src/main/java/com/choi/jajaotalk/JajaotalkApplication.java
@@ -0,0 +1,13 @@
+package com.choi.jajaotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JajaotalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JajaotalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choi/jajaotalk/JajaotalkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.choi.jajaotalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JajaotalkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
